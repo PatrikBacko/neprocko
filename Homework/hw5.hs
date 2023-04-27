8185d6f054be98338b352d035c09c8f2e5459a28
Patrik Backo <llama8989@gmail.com>
2023-04-27 11:01:44 +0200

    05 HW DONE aj deletion

--- Homework/hw5.hs
+++ Homework/hw5.hs
@@ -134,8 +134,8 @@ delete ks t
 
 delete' :: (Ord k) => [k] -> Trie k v -> Trie k v
 delete' [] (ValueNode _ children) = Node children
-delete' (k:ks) (Node children) = Node (cleaup (processChildren (k:ks) children []))
-delete' (k:ks) (ValueNode value children) = ValueNode value (cleaup (processChildren (k:ks) children []))
+delete' (k:ks) (Node children) = Node (cleanUp (processChildren (k:ks) children []))
+delete' (k:ks) (ValueNode value children) = ValueNode value (cleanUp (processChildren (k:ks) children []))
 
 processChildren :: (Ord k) => [k] -> [(k, Trie k v)] -> [(k, Trie k v)] -> [(k, Trie k v)]
 processChildren [k] ((key, ValueNode value []):children) acc
@@ -148,11 +148,11 @@ processChildren (k:ks) ((key, Node rest):children) acc
     | k == key = reverse acc ++ (key, delete' ks (Node rest)):children
     | otherwise = processChildren (k:ks) children ((key, Node rest):acc)
 
-cleaup :: (Ord k) => [(k, Trie k v)] -> [(k, Trie k v)]
-cleaup [] = []
-cleaup ((key, Node []):children) = cleaup children
-cleaup ((key, Node rest):children) = (key, Node rest):(cleaup children)
-cleaup ((key, ValueNode value rest):children) = (key, ValueNode value rest):(cleaup children)
+cleanUp :: (Ord k) => [(k, Trie k v)] -> [(k, Trie k v)]
+cleanUp [] = []
+cleanUp ((key, Node []):children) = cleanUp children
+cleanUp ((key, Node rest):children) = (key, Node rest):(cleanUp children)
+cleanUp ((key, ValueNode value rest):children) = (key, ValueNode value rest):(cleanUp children)
 
 --reverse acc nemusíme použiť pokiaľ nám nezáleží na poradí kľúčov, ale kvôli debugovaniu som si to implementoval, aby som mohol porovnávať s fromList
 
