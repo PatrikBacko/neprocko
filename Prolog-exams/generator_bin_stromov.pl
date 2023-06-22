4f286a4af5e584f2955127f9ca9c4440d0950c8a
Buciak <patrik.backo1@gmail.com>
2023-06-22 18:19:06 +0200

    JEBEM CELÝ KOKOTSKÝ STROM, FAKT DOPIČE NEVIEM ČO S TÝM

--- Prolog-exams/generator_bin_stromov.pl
+++ Prolog-exams/generator_bin_stromov.pl
@@ -10,16 +10,17 @@ maptree(P, b(T1, V, T2)) :-
 
 % size(-Tree, +Number_of_nodes, +Height)
 
+size(nil, 0, _):- !.
+% size(b(nil, _, nil), 1, 1):- !.
+
+
 size(b(T1, _, T2), N, H1) :-
     N > 0,
     H1 > -1,
     NN is N-1,
-    NN >= H1,
+    N > H1,
     between(0, NN, V),
     H2 is H1 - 1,
     V1 is NN - V,
     size(T2, V1, H2),
-    size(T1, V, H2). 
-
-size(nil, 0, -1):- !.
-size(nil, 0, _):- !.
+    size(T1, V, H2).
