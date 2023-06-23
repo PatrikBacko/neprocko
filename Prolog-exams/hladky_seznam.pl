71c5cfea4c81711fb6498387a65479a2673287a8
Buciak <patrik.backo1@gmail.com>
2023-06-23 15:32:18 +0200

    NOW HLADKY ZOZNAM A MATICA DONEEE

--- Prolog-exams/hladky_seznam.pl
+++ Prolog-exams/hladky_seznam.pl
@@ -1,51 +1,59 @@
+%test(?X, ?Y) testuje či je zoznam [X,Y] hladký
 test(X,Y) :- 
-    nonvar(X),
-    nonvar(Y),
-    integer(X),
-    integer(Y),
-    X1 is X-1,
-    X2 is X+1,
-    between(X1, X2, Y),
-    !. 
+	nonvar(X),
+	nonvar(Y),
+	integer(X),
+	integer(Y),
+	X1 is X-1,
+	X2 is X+1,
+	between(X1, X2, Y),
+	!. 
 
 test(X, Y) :-
-    var(Y), !,
-    integer(X),
-    X1 is X-1,
-    X2 is X+1,
-    between(X1, X2, Y).
+	var(Y), !,
+	integer(X),
+	X1 is X-1,
+	X2 is X+1,
+	between(X1, X2, Y).
 
 test(X, Y) :-
-    var(X), !,
-    integer(Y),
-    Y1 is Y-1,
-    Y2 is Y+1,
-    between(Y1, Y2, X).
-    
+	var(X), !,
+	integer(Y),
+	Y1 is Y-1,
+	Y2 is Y+1,
+	between(Y1, Y2, X).
+	
 
-%hladky(?Xs)
+%hladky(?Xs) testuje a generuje hladké zoznamy
 hladky([]).
 hladky([_]).
 hladky([X,Y|Xs]) :-
-    hladky([Y|Xs]),
-    test(X,Y).
+	((nonvar(X); nonvar(Y)) -> 
+	test(X,Y),
+	hladky_([X,Y|Xs]);
 
+	hladky([Y|Xs]),
+	test(X,Y)
+	).
+
+hladky_([]).
+hladky_([_]).
+hladky_([X, Y|Xs]) :-
+	test(X, Y),
+	hladky_([Y|Xs]).
+
+%hladka(?Xs) - generuje a testuje hladké matice
 hladka(Xs) :- 
-    use_module(library(clpfd)),
-    transpose(Xs, Ys),
-    hladka_(Xs, Ys).
+	use_module(library(clpfd)),
+	transpose(Xs, Ys),
+	hladka_(Xs, Ys).
 
-hladka_([], []).
+hladka_([X|Xs], Ys) :-
+	hladky(X),
+	hladka__(Ys),
+	hladka__(Xs).
 
-hladka_([X|Xs], []) :-
-    hladky(X),
-    hladka_(Xs, []).
-
-hladka_([], [X|Xs]) :-
-    hladky(X),
-    hladka_(Xs, []).
-
-hladka_([X|Xs], [Y|Ys]) :-
-    hladky(X),
-    hladky(Y),
-    hladka_(Xs, Ys).
+hladka__([]).
+hladka__([X|Xs]) :-
+	hladky(X),
+	hladka__(Xs).
