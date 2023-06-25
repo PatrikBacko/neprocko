c14b67664c37f8253d17a799d639fc566604110d
Buciak <patrik.backo1@gmail.com>
2023-06-25 15:05:47 +0200

    najdlhšia cesta - úprava

--- Haskell exams/najdlhsia_cesta.hs
+++ Haskell exams/najdlhsia_cesta.hs
@@ -19,22 +19,26 @@ g = [   ("a", [("b",1), ("c", 1), ("f", 3)]),
 
 -- nc :: Graf -> String -> String -> Maybe ([String], Int)
 nc :: (Num n, Eq a, Ord n) => [(a, [(a, n)])] -> a -> a -> Maybe ([a], n)
-nc g u v 
-    | result == [] = Nothing
-    | otherwise = Just (reverse $ fromJust $ lookup m result, m)
-        where 
-            result = map (\(a,n) -> (n,a)) (findRoutes g u v [] 0)
-            m = maximum (map fst result)
+nc g u v = solution $ findRoutes g u v [] 0
+    where
+        solution [] = Nothing
+        solution ((n, c):rs) = Just (reverse c, n)
 
-findRoutes :: (Num n, Eq a) => [(a, [(a, n)])] -> a -> a -> [a] -> n -> [([a], n)]
-findRoutes g u v c l = concatMap (fRoute gNew v (u:c) l) ns
+
+findRoutes :: (Num n, Eq a, Ord n) => [(a, [(a, n)])] -> a -> a -> [a] -> n -> [(n, [a])]
+findRoutes g u v c l = longestRoute $ concatMap (fRoute gNew v (u:c) l) ns
     where 
         ns = findNeighbours g u
         gNew = filter (\(a,b) -> a /= u) g 
 
         fRoute g v c l (u,i) 
-            | v == u = [(u:c, l+i)]
+            | v == u = [(l+i, u:c)]
             | otherwise = findRoutes g u v c (l+i)
+
+        longestRoute [] = []
+        longestRoute rs = [(m, reverse $ fromJust $ lookup m rs)]
+            where 
+                m = maximum (map fst rs)
 
 
 findNeighbours :: Eq a => [(a, [b])] -> a -> [b]
